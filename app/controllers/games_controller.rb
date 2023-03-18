e2e057f3f01d983ed93d8b6dad799450139e46ee
Robert Low <robert.low91@gmail.com>
2023-03-18 19:06:03 +0000

    letters form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,10 @@
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U)
+
   def new
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
   def score
